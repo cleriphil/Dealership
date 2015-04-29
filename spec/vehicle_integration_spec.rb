8fe6638c30e3bfe806a2810562fe323a51fd45d0
cleri philiastides & steve schneider <student@epicodus.com>
2015-04-29 10:52:09 -0700

    Add and pass integration spec to view vehicle list

--- spec/vehicle_integration_spec.rb
+++ spec/vehicle_integration_spec.rb
@@ -4,7 +4,7 @@ Capybara.app = Sinatra::Application
 set(:show_expections, false)
 
 describe('path for Vehicle class', {:type => :feature}) do
-  it('processes the user input and') do
+  it('processes the user input and returns the success page') do
     visit('/')
     click_link('Add New Vehicle')
     fill_in('make', :with => 'Toyota')
@@ -13,4 +13,15 @@ describe('path for Vehicle class', {:type => :feature}) do
     click_button('Add Vehicle')
     expect(page).to have_content('Success!')
   end
+  it('processes the user input and returns the vehicles page') do
+    visit('/')
+    click_link('Add New Vehicle')
+    fill_in('make', :with => 'Toyota')
+    fill_in('model', :with => 'Prius')
+    fill_in('year', :with => 2000)
+    click_button('Add Vehicle')
+    click_link('See Vehicle List')
+    expect(page).to have_content('Here are the vehicles on this lot:')
+  end
+
 end
